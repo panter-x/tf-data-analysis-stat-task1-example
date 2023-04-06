9443d5c96b51d5bd095afe9901a61899701716e9
panter-x <129738391+panter-x@users.noreply.github.com>
2023-04-06 20:11:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 46951859 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    lamb = np.mean(x) / 34
+    return lamb # Ваш ответ
